0cd540fc3a18ec6ddbafda50310ba9207af23f8a
arshathahamed10 <arshathahamed500@gmail.com>
2024-11-02 21:56:25 +0530

    ADded

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["https://deploy-mern"],
+    origin: ["https://idea-collector-front.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
