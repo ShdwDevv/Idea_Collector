3377a1b07804ad55d10b24b166f672763f3f925a
arshathahamed10 <arshathahamed500@gmail.com>
2024-11-04 11:32:30 +0530

    AAA

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://idea-collector.onrender.com"
+    process.env.CORS_ORIGIN
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
